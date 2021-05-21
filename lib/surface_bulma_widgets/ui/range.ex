e9f80cdc1dfc7b88826de0a60ba28ab7926bb8e2
Jaremy J. Creechley <jaremy.creechley@wavebaselabs.com>
2021-05-21 04:10:00 -0700

    adding range

---
+++ lib/surface_bulma_widgets/ui/range.ex
@@ -0,0 +1,40 @@
+defmodule SurfaceBulmaWidgets.UI.Range do
+  use Surface.LiveComponent
+  alias SurfaceBulma.Button
+
+  use SurfaceBulmaWidgets
+  alias SurfaceBulmaWidgets.Components.Progress
+
+  require Logger
+
+  # prop key, :atom, required: true
+  prop count, :tuple, default: {nil, 0}
+  prop channel, :string, default: nil
+
+  # data count, :integer, default: 0
+
+  def render(assigns) do
+    ~H"""
+    <div class="buttons has-addons is-centered">
+      <Button rounded>{{ value(@count) }}</Button>
+      <Button rounded>{{ key(@count) }}</Button>
+      <Progress id="range" value={{ value(@count) }} classes={{ ["is-info"] }} />
+      <Button click="incr" rounded>+</Button>
+      <Button click="decr" rounded>-</Button>
+    </div>
+    """
+  end
+
+  def handle_event("incr", _, socket) do
+    # Logger.warn("clicked 'incr' (#{inspect(socket.assigns.id)})")
+    socket |> binding_update(:count, &(&1 + 1))
+    {:noreply, socket}
+  end
+
+  def handle_event("decr", _, socket) do
+    # Logger.warn("clicked 'decr'")
+    socket |> binding_update(:count, &(&1 -1))
+
+    {:noreply, socket}
+  end
+end
