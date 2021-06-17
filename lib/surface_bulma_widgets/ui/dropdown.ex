8f7848b1fade7a4892931bbbc47317a80e33ff5d
Jaremy J. Creechley <jaremy.creechley@wavebaselabs.com>
2021-06-16 17:40:43 -0700

    kind of workings

--- lib/surface_bulma_widgets/ui/dropdown.ex
+++ lib/surface_bulma_widgets/ui/dropdown.ex
@@ -10,9 +10,12 @@ defmodule SurfaceBulmaWidgets.UI.Dropdown do
 
   prop prefix, :string
   prop items, :list, default: []
-  prop items_type, :atom, values: [:string, :integer, :float]
+  # prop kind, :atom, values: [:string, :integer, :float]
+  prop integers, :boolean, default: false
+  prop floats, :boolean, default: false
 
   prop var, :tuple, default: {nil, 0}
+  prop channel, :string, default: nil
 
 
   def render(assigns) do
@@ -34,7 +37,7 @@ defmodule SurfaceBulmaWidgets.UI.Dropdown do
           <div class="dropdown-content">
             <a href="#" :for={{ item <- @items }}
                :on-click="select"
-               phx-value-name5="{{item}}"
+               phx-value-value="{{item}}"
                class={{"dropdown-item", "is-active": item == value(@var) }}>
               {{item}}
             </a>
@@ -47,7 +50,27 @@ defmodule SurfaceBulmaWidgets.UI.Dropdown do
 
   def handle_event("select", data, socket) do
     Logger.warn("dropwdown selected: #{inspect data }")
+    v! = value_parser(socket, data)
+
+    socket |> binding_update(:var, fn _v -> v! end)
     {:noreply, socket}
   end
 
+  def value_parser(%{assigns: assigns}, %{"value" => data}, default \\ nil) do
+    Logger.warn("dropwdown selected: parsed: floats: #{inspect assigns[:floats]}")
+    Logger.warn("dropwdown selected: parsed: integers: #{inspect assigns[:integers]}")
+    cond do
+      assigns[:floats] ->
+        case data |> Float.parse() do
+          {number, _rest} -> number
+          :error -> default or :error
+        end
+      assigns[:integers] ->
+        case data |> Integer.parse() do
+          {number, _rest} -> number
+          :error -> default or :error
+        end
+      true -> data
+    end
+  end
 end
