467b8d08044b33c89dd028af534901c502aa0cb3
Jaremy J. Creechley <jaremy.creechley@wavebaselabs.com>
2021-07-05 01:20:00 -0700

    remove loggers

--- lib/surface_bulma_widgets/ui/numbers/number_utils.ex
+++ lib/surface_bulma_widgets/ui/numbers/number_utils.ex
@@ -33,27 +33,22 @@ defmodule SurfaceBulmaWidgets.UI.NumberUtils do
   end
 
   def format(value, digits) when is_integer(digits) and is_integer(value) do
-    Logger.info("FMT: digits: #{inspect digits}")
     fformat(value, [digits, 0])
   end
 
   def format(value, digits) when is_integer(digits) and is_float(value) do
-    Logger.info("FMT: digits: #{inspect digits}")
     fformat(value, [0, digits])
   end
 
   defp fformat(value, [characteristic, 0]) do
-    Logger.info("FMT: fdigits: #{inspect [characteristic, 0]}")
     :io_lib.format("~#{characteristic}.. B", [value |> round])
   end
 
   defp fformat(value, [0, mantissa]) do
-    Logger.info("FMT: fdigits: 0. #{inspect [0, mantissa]}")
     :io_lib.format("~.#{mantissa}f",[1.0*value])
   end
 
   defp fformat(value, [characteristic, mantissa]) do
-    Logger.info("FMT: fdigits: #{inspect [characteristic, mantissa]}")
     :io_lib.format("~#{characteristic}.#{mantissa}f",[1.0*value])
   end
 
